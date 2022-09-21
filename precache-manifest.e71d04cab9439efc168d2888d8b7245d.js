35f319df007678ecf77002f7d8247e6d22f7c6f3
Tenzin Kalden <tenzinkalden@Tenzins-MacBook-Air.local>
2022-09-21 15:02:18 -0400

    Updates

---
+++ precache-manifest.e71d04cab9439efc168d2888d8b7245d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "31fd3d6f81ead16ff698",
+    "url": "/static/css/main.d9f578cb.chunk.css"
+  },
+  {
+    "revision": "31fd3d6f81ead16ff698",
+    "url": "/static/js/main.31fd3d6f.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fd9cd4e0eef0068a35cc9d50937e1684",
+    "url": "/index.html"
+  }
+];
