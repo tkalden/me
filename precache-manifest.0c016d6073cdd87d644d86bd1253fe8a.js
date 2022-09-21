039289532aacc6d7c1a24a2d0df688fed5872c8e
Tenzin Kalden <tenzinkalden@Tenzins-MacBook-Air.local>
2022-09-21 15:28:47 -0400

    Updates

---
+++ precache-manifest.0c016d6073cdd87d644d86bd1253fe8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a815d3eaa48c34daff4",
+    "url": "/my-portfolio-app/static/css/main.d9f578cb.chunk.css"
+  },
+  {
+    "revision": "0a815d3eaa48c34daff4",
+    "url": "/my-portfolio-app/static/js/main.0a815d3e.chunk.js"
+  },
+  {
+    "revision": "ad2b29cdfb06374227e2",
+    "url": "/my-portfolio-app/static/js/1.ad2b29cd.chunk.js"
+  },
+  {
+    "revision": "d44645bfb3fd5322dac6",
+    "url": "/my-portfolio-app/static/js/runtime~main.d44645bf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-portfolio-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4841feba27b7b7792ca32dec58783210",
+    "url": "/my-portfolio-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53a696763481c05b4ce4145abffb930b.js"
+  "/my-portfolio-app/precache-manifest.0c016d6073cdd87d644d86bd1253fe8a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-portfolio-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
